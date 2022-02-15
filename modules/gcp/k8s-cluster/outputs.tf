0be02d0c8affeba2c386f10b61208e013e04d1ca
Edwin <edwin.pena@pyxis.com.uy>
2022-02-14 22:54:18 -0300

    .

--- modules/gcp/k8s-cluster/outputs.tf
+++ modules/gcp/k8s-cluster/outputs.tf
@@ -2,9 +2,9 @@ output "project_id" {
   value = var.project_id
 }
 
-output "region" {
-  value = module.gke.region
-}
+# output "region" {
+#   value = module.gke.region
+# }
 
 output "cluster" {
   value = module.gke.name
--- modules/gcp/k8s-cluster/providers.tf
+++ modules/gcp/k8s-cluster/providers.tf
@@ -1,4 +1,5 @@
 provider "google" {
   project = var.project_id
-  region  = var.region
+  region                     = "us-central1"
+  # region  = var.region
 }
