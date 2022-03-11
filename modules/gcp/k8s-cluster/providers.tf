013cfac5b71d26d64be446619e178bb0c26a7573
Edwin Peña <edwin.rea3@gmail.com>
2022-03-11 19:09:46 -0300

    Delete modules/gcp/k8s-cluster directory

--- modules/gcp/k8s-cluster/providers.tf
+++
@@ -1,5 +0,0 @@
-provider "google" {
-  project = var.project_id
-  region                     = "us-central1"
-  # region  = var.region
-}
--- modules/gcp/k8s-cluster/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "project_id"               {}
-# variable "region"                   {}
-variable "machine_type"             {}
-variable "max_nodes"                {}
-variable "environment"              {}
-variable "master_cidr"              {}
-variable "subnet_primary_ip_range"  {}
-variable "subnet_pods_ip_range"     {}
-variable "subnet_services_ip_range" {}
