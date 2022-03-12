556f75bada5ed346faf9ad3d397bcd0c17e4b052
Edwin <edwin.pena@alphalabs.uy>
2022-03-11 21:33:30 -0300

    code clean

--- environments/stg/vpc/.terragrunt-cache/leUyZ0aBqaFptlTSlg0hcwjScx4/to05eEERff8UmWYG2fZwhswIA5I/modules/vpc/terragrunt.hcl
+++
@@ -1,7 +0,0 @@
-terraform {
-  source = "../../..//modules/vpc/"
-}
-
-include {
-  path = find_in_parent_folders()
-}
--- environments/stg/vpc/.terragrunt-cache/leUyZ0aBqaFptlTSlg0hcwjScx4/to05eEERff8UmWYG2fZwhswIA5I/modules/vpc/variables.tf
+++
@@ -1,7 +0,0 @@
-variable "org"                      {}
-variable "subnet_primary_ip_range"  {}
-variable "subnet_pods_ip_range"     {}
-variable "subnet_services_ip_range" {}
-variable "subnet_ip"                {}
-variable "project_id"               {}
-variable "region"                   {}
