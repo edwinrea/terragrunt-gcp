bdc498c18d60c061f5b6af97a47298fe23e72a18
Edwin P <edwin.rea3@gmail.com>
2022-08-02 14:08:58 -0300

    Pruebas

--- test/modules/vpc/variables.tf
+++ test/modules/vpc/variables.tf
@@ -3,5 +3,7 @@ variable "subnet_primary_ip_range" {}
 variable "subnet_pods_ip_range" {}
 variable "subnet_services_ip_range" {}
 variable "subnet_ip" {}
-variable "project_id" {}
+variable "project_id" {
+    default = "alpha-siged-education-pro"
+}
 variable "region" {}
