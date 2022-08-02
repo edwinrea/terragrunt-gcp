6dedb2d9b0507945c2a49e1a45e71c32beb0d0a3
Edwin P <edwin.rea3@gmail.com>
2022-08-02 15:10:18 -0300

    Pruebas

---
+++ terraform/main.tf
@@ -0,0 +1,18 @@
+provider "google" {
+  credentials = get_env("GCP_CREDENTIAL", "")
+  project     = "alpha-siged-education-pro"
+  region      = "us-central1"
+  zone        = "us-central1-a"
+}
+
+# resource "google-compute_network" "vpc_network" {
+#   name = "test_terraform_vpc"
+# }
+
+
+resource "google_compute_network" "vpc_network" {
+  project                 = "alpha-siged-education-pro"
+  name                    = "vpc-network"
+  auto_create_subnetworks = true
+  mtu                     = 1460
+}
